c99bbd0dabd48b767b7ad5628eee127a08da3561
zsy-win10 <zhangshiye979@gmail.com>
2018-10-08 12:38:21 +0800

    ..

--- RemoveDuplicates.py
+++ RemoveDuplicates.py
@@ -1,7 +1,15 @@
-#https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
-nums = [1,1,2]
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+nums = [0, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 5, 6]
 
-for num in nums[:]:
-    while num in nums:
-        nums.remove(num)
-
+i = 0
+j = 0
+while j < len(nums):
+    if nums[i-1] != nums[j]:
+        nums[i] = nums[j]
+        i += 1
+        j += 1
+    # 相等的时候
+    else:
+        j += 1
+print(min(i, len(nums)))
+print(nums)
